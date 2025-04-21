36c86cd5088d6ac96dfbcf3e6ca8ceae438be2cd
Emanuel Pecson <emanpecson@gmail.com>
2025-04-20 20:52:32 -0700

    feat: :sparkles: setup clipping functionality

--- src/main/java/com/hooparchives/Clipper.java
+++ src/main/java/com/hooparchives/Clipper.java
@@ -1,5 +1,6 @@
 package com.hooparchives;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
@@ -11,6 +12,7 @@ public class Clipper {
 	private final S3TransferManager manager;
 	private final String bucket = "hoop-archives-uploads";
 	private final String downloadPath = "downloads";
+	private final String clipOutputPath = "clips";
 
 	public Clipper() {
 		this.manager = S3TransferManagerProvider.getTransferManager();
@@ -27,5 +29,40 @@ public class Clipper {
 		CompletedFileDownload downloadResult = downloadFile.completionFuture().join();
 		System.out.println("Content length: " + downloadResult.response().contentLength());
 		return downloadResult.response().contentLength();
+	}
+
+	public void trimVideo(String filename, String startTime, String duration) {
+		String inputFilePath = this.downloadPath + "/" + filename;
+		System.out.println("Input file path: " + inputFilePath);
+
+		// perfrom trim request
+		ProcessBuilder processBuilder = new ProcessBuilder(
+				"ffmpeg",
+				"-i", inputFilePath,
+				"-ss", startTime,
+				"-t", duration,
+				"-c", "copy", // copy codecs (w/o re-encoding)
+				Paths.get(this.clipOutputPath, filename).toString());
+
+		try {
+			Process process = processBuilder.start();
+			int exitCode = process.waitFor();
+
+			if (exitCode == 0) {
+				System.out.println("✅ Video trimmed successfully!");
+			} else {
+				System.out.println("❌ Error trimming video. Exit code: " + exitCode);
+			}
+		} catch (IOException e) {
+			System.out.println("❗ IOException occurred: " + e.getMessage());
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			System.out.println("⛔ Interrupted while trimming video: " + e.getMessage());
+			Thread.currentThread().interrupt(); // restore interrupt status
+		}
+
+		// ? upload to ddb (later?)
+
+		// TODO: upload to s3
 	}
 }
